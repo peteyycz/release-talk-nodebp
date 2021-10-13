011884577a510755cc76eddd6eef1c3432245df6
Peter Czibik <peter.czibik@risingstack.com>
2021-10-13 19:24:25 +0200

    feat: change return value
    BREAKING CHANGE: no longer 1 but 2

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,7 +2,7 @@ const mod = require('.');
 const { expect } = require('chai');
 
 describe('my module', () => {
-  it('should return 1', () => {
-    expect(mod()).to.equal(1);
+  it('should return 2', () => {
+    expect(mod()).to.equal(2);
   });
 });
